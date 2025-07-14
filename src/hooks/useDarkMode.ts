6679166f41fb37657f0b2c61fe84ef58a0a011f0
GrecoPeres <grecorainbow@gmail.com>
2025-07-14 15:49:05 -0300

    feat: I am dev the mode dark/light using with propriety tailwind

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+export function useDarkMode() {
+  const [isDark, setIsDark] = useState(() =>
+    localStorage.getItem('theme') === 'dark' ||
+    (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (isDark) {
+      root.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      root.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [isDark]);
+
+  const toggleDark = () => setIsDark(!isDark);
+
+  return { isDark, toggleDark };
+}
