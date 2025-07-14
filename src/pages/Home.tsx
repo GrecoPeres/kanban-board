930263d010bbe6f42d514e5c8a9ef3b89565fe7f
GrecoPeres <grecorainbow@gmail.com>
2025-07-14 10:47:29 -0300

    feat: I importing the new features to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import BoardColumn from '../components/BoardColumn';
 import FloatingButton from '../components/FloatingButton';
 import TaskModal from '../components/TaskModal';
+import type { Task } from '../types';
 
 const sampleTasks = {
-  'To-do': [
+  'Em fila': [
     {
       id: '1',
       title: 'Criação de wireframes',
@@ -66,19 +67,62 @@ const sampleTasks = {
 };
 
 export default function Home() {
-  const [data] = useState(sampleTasks);
+
+  const [data, setData] = useState(() => {
+    const saved = localStorage.getItem('kanbanData');
+    return saved ? JSON.parse(saved) : sampleTasks;
+  });
+
+  const addTask = (newTask: any) => {
+    const updated = {
+      ...data,
+      'Em fila': [...data['Em fila'], newTask],
+    };
+    setData(updated);
+    localStorage.setItem('kanbanData', JSON.stringify(updated));
+  };
+
+  const handleSaveTask = (newTask: Task) => {
+    const updated = { ...data };
+    const column = 'Em fila';
+  
+    const taskIndex = updated[column].findIndex((t: Task) => t.id === newTask.id);
+    if (taskIndex >= 0) {
+      updated[column][taskIndex] = newTask;
+    } else {
+      updated[column].push(newTask);
+    }
+  
+    setData(updated);
+    localStorage.setItem('kanbanData', JSON.stringify(updated));
+  };  
+  
   const [isModalOpen, setModalOpen] = useState(false);
+  const [taskToEdit, setTaskToEdit] = useState<Task | null>(null);
+
   return (
     <>
     <div className="min-h-screen bg-gray-100 dark:bg-gray-950 p-6">
       <div className="flex flex-row gap-6 overflow-x-auto">
         {Object.entries(data).map(([status, tasks]) => (
-          <BoardColumn key={status} title={status} tasks={tasks} />
+          <BoardColumn key={status} title={status} tasks={tasks} onEdit={(task) => {
+            setTaskToEdit(task);
+            setModalOpen(true);
+          }} />
         ))}
       </div>
     </div>
     <FloatingButton onClick={() => setModalOpen(true)} />
-      <TaskModal isOpen={isModalOpen} onClose={() => setModalOpen(false)} />
+    <TaskModal
+      isOpen={isModalOpen}
+      onClose={() => {
+        setModalOpen(false);
+        setTaskToEdit(null);
+      }}
+      onSave={handleSaveTask}
+      taskToEdit={taskToEdit}
+    />
+
     </>
   );
 }
