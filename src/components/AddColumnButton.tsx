f3856e0e111268b3924bf7608380bf91be0bbc63
GrecoPeres <grecorainbow@gmail.com>
2025-07-15 15:55:53 -0300

    feat: I dev new component for add new column in kanban

---
+++ src/components/AddColumnButton.tsx
@@ -0,0 +1,18 @@
+// src/components/AddColumnButton.tsx
+interface AddColumnButtonProps {
+    onClick: () => void;
+  }
+  
+  export default function AddColumnButton({ onClick }: AddColumnButtonProps) {
+    return (
+      <div
+        onClick={onClick}
+        className="w-72 min-h-[120px] flex items-center justify-center border-2 border-dashed border-gray-300 dark:border-gray-600 rounded-xl cursor-pointer hover:border-blue-500 hover:bg-blue-50 dark:hover:bg-gray-800 transition"
+      >
+        <button className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 font-medium">
+          + Adicionar Coluna
+        </button>
+      </div>
+    );
+  }
+  
